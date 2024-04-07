eabce044da100ef90b7c08b710280c338a3a86a5
Wojciech Sęk <wojciech.sek17@gmail.com>
2024-04-07 18:05:10 +0200

    getting all of the data

--- part_2/worker/src/main.py
+++ part_2/worker/src/main.py
@@ -1,33 +1,51 @@
-import time
+import time, os, sys, json
 
 from config import get_config
-from aws import get_sqs_client
+from aws import get_sqs_client, get_s3_client
 
-def  get_debts_id(sqs_client):
-    msg = sqs_client.receive_message(
+def get_debts_id(sqs_client) -> str:
+    response = sqs_client.receive_message(
                 QueueUrl=config.worker_queue_url,
                 MaxNumberOfMessages=1,
                 WaitTimeSeconds=1,
             )
+    
+    msgs = response["Messages"]
+    msg = msgs[0]
+    body = msg["Body"]
+    json_body = json.loads(body)
+    debts_id = json_body["debts_id"]
 
-    return msg["Messages"][0]["Body"]["debts_id"]
+    return debts_id
 
-def get_debts(debts_id):
-    pass
+def get_debts(debts_id, s3_client):
+    return s3_client.get_object(
+        Bucket=config.debts_bucket_name,
+        Key=debts_id
+    )
 
 if __name__ == "__main__":
     config = get_config()
 
     while True:
         sqs_client = get_sqs_client()
+        s3_client = get_s3_client()
         
         try:
             debts_id = get_debts_id(sqs_client)
-            print(f"Got debts_id: {debts_id}")
+            print("Got debts_id!!!")
 
+            debts_info = get_debts(debts_id, s3_client)
+            print("Got debts_info!!!")
+
+            print(debts_info)
 
 
 
         except Exception as e:
-            print("Got an error: ", e)    
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
+            print(e)
+
             time.sleep(1)
